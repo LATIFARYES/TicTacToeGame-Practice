449077006ef13c97da699282ac45bf693f17a99f
LATIFARYES <latifaryes@gmail.com>
2024-05-06 15:07:07 +0300

    Initial Commit

---
+++ TicTacToeGame/TicTacToeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeGameApp.swift
+//  TicTacToeGame
+//
+//  Created by LATIFA on 06/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
