3d5e8622fcf9d7934bbc2b3aa35d4916fbff3c69
LATIFARYES <latifaryes@gmail.com>
2024-05-06 17:50:15 +0300

    Final Game

--- TicTacToeGame/ContentView.swift
+++ TicTacToeGame/ContentView.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var TicTac = TicTacModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            Text("TIC TAC TOE 👾")
+                .font(.title)
+                .bold()
+            
+            let col = Array(repeating: GridItem( .flexible()), count: 3)
+            
+            LazyVGrid(columns: col, content: {
+                ForEach(0..<9) { i in
+                    Button(action: {
+
+                        TicTac.buttonTap(i: i)
+                        
+                    }, label: {
+                        Text(TicTac.buttonLabel(i: i))
+                            .frame(width: 100, height: 100)
+                            .background(.blue)
+                            .foregroundStyle(.white)
+                            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                            .bold()
+                    
+                    })
+                    
+                }
+            })
+            .padding(.bottom)
+            
+            Button(action: {
+                
+                TicTac.resetGame()
+                
+            }, label: {
+                Text("RESTART GAME ♺")
+                    .foregroundColor(.green)
+                    .font(.title3)
+                    .bold()
+                    
+                
+                
+            })
+            
         }
+        
+        
         .padding()
     }
 }
